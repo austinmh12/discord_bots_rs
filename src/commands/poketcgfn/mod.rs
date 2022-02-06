e2891546b779e09acdf2e1ec0277801d9dd60c9c
Austin Heil <austinmh12@gmail.com>
2022-02-06 17:41:17 -0500

    Learning how to make crates

--- src/commands/poketcgfn/mod.rs
+++ src/commands/poketcgfn/mod.rs
@@ -5,7 +5,7 @@ fn api_call(endpoint: String, params: Vec<Tuple<String, String>>) -> serde_json:
 	dotenv::dotenv().ok();
 	let poketcg_key = dotenv::var("POKETCGAPIKEY").unwrap();
 	return reqwest::Client::new()
-	.get(format!("https://api.pokemontcg.io/v2/{}", endpoint))
+		.get(format!("https://api.pokemontcg.io/v2/{}", endpoint))
 		.header("X-Api-Key", poketcg_key)
 		.query(&params)
 		.send()?
