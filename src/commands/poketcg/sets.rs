9eee1226c85bdac901e81a7aef9c22830bec0a63
Austin Heil <austinmh12@gmail.com>
2022-03-31 20:03:16 -0400

    Adding set stuff:

---
+++ src/commands/poketcg/sets.rs
@@ -0,0 +1,10 @@
+pub struct Set {
+	pub id: String,
+	pub name: String,
+	pub series: String,
+	pub printed: i32,
+	pub total: i32,
+	pub logo: String,
+	pub symbol: String,
+	pub releaseDate: String // Will be a datetime eventually?
+}
