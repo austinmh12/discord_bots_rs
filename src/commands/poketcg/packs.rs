4080c1f0293381f24c2fe9c8bb4b59cdd61137b7
Austin Heil <austinmh12@gmail.com>
2022-04-17 20:27:56 -0400

    Fixed issue with promo packs not working

--- src/commands/poketcg/packs.rs
+++ src/commands/poketcg/packs.rs
@@ -70,31 +70,43 @@ impl Pack {
 			.filter(|c| c.rarity == "Common")
 			.map(|c| c.to_owned())
 			.collect::<Vec<Card>>();
+		let promos = all_cards
+			.iter()
+			.filter(|c| c.rarity == "Promo")
+			.map(|c| c.to_owned())
+			.collect::<Vec<Card>>();
 		let mut cards = vec![];
-		if commons.len() > 0 {
-			for _ in 0..6*amount {
-				let c = commons.choose(&mut thread_rng()).unwrap().clone();
+		if commons.len() > 0 && uncommons.len() > 0 && rares.len() > 0 {
+			if commons.len() > 0 {
+				for _ in 0..6*amount {
+					let c = commons.choose(&mut thread_rng()).unwrap().clone();
+					cards.push(c);
+				}
+			}
+			if uncommons.len() > 0 {
+				for _ in 0..3*amount {
+					let c = uncommons.choose(&mut thread_rng()).unwrap().clone();
+					cards.push(c);
+				}
+			}
+			let mut rares_with_weights = vec![];
+			for rare in rares {
+				let weight = RARITY_MAPPING.get(rare.rarity.as_str()).unwrap_or(&0).to_owned();
+				rares_with_weights.push((rare, weight));
+			}
+			for _ in 0..amount {
+				let c = rares_with_weights
+					.choose_weighted(&mut thread_rng(), |rw| rw.1 as i32)
+					.unwrap()
+					.clone()
+					.0;
 				cards.push(c);
 			}
-		}
-		if uncommons.len() > 0 {
-			for _ in 0..3*amount {
-				let c = uncommons.choose(&mut thread_rng()).unwrap().clone();
+		} else {
+			for _ in 0..amount {
+				let c = promos.choose(&mut thread_rng()).unwrap().clone();
 				cards.push(c);
 			}
-		}
-		let mut rares_with_weights = vec![];
-		for rare in rares {
-			let weight = RARITY_MAPPING.get(rare.rarity.as_str()).unwrap_or(&0).to_owned();
-			rares_with_weights.push((rare, weight));
-		}
-		for _ in 0..amount {
-			let c = rares_with_weights
-				.choose_weighted(&mut thread_rng(), |rw| rw.1 as i32)
-				.unwrap()
-				.clone()
-				.0;
-			cards.push(c);
 		}
 
 		Ok(Self {
