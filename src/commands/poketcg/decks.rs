cb0f4284d9414de517848252a24569341372ae92
Austin Heil <austinmh12@gmail.com>
2022-06-13 20:45:54 -0400

    Added deck database functions

--- src/commands/poketcg/decks.rs
+++ src/commands/poketcg/decks.rs
@@ -1,3 +1,4 @@
+use futures::TryStreamExt;
 use serde::{Serialize, Deserialize};
 use mongodb::{
 	bson::{
@@ -8,6 +9,8 @@ use mongodb::{
 	Collection
 };
 use serenity::{builder::CreateEmbed, utils::Colour};
+
+use crate::commands::get_client;
 
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
@@ -29,17 +32,57 @@ impl Deck {
 		}
 	}
 
-	pub fn to_doc(&self) -> Document {
-		let mut d = Document::new();
-		d.insert("id", &self.id);
-		d.insert("discord_id", &self.discord_id);
-		d.insert("name", &self.name);
-		d.insert("cards", &self.cards);
-
-		d
-	}
-
 	pub fn is_valid(&self) -> bool {
 		self.cards.len() == 60 as usize
 	}
+}
+
+async fn get_deck_collection() -> Collection<Deck> {
+	let client = get_client().await.unwrap();
+	let collection = client.database("poketcg").collection::<Deck>("decks");
+
+	collection
+}
+
+async fn add_deck(deck: &Deck) {
+	let deck_collection = get_deck_collection().await;
+	deck_collection
+		.insert_one(deck, None)
+		.await
+		.unwrap();
+}
+
+async fn get_decks_by_player(discord_id: i64) -> Vec<Deck> {
+	let deck_collection = get_deck_collection().await;
+	let decks = deck_collection
+		.find(doc! { "discord_id": discord_id }, None)
+		.await
+		.unwrap()
+		.try_collect::<Vec<Deck>>()
+		.await
+		.unwrap();
+
+	decks
+}
+
+async fn get_deck(discord_id: i64, name: String) -> Option<Deck> {
+	let deck_collection = get_deck_collection().await;
+	let deck = deck_collection
+		.find_one(doc! { "discord_id": discord_id, "name": name }, None)
+		.await
+		.unwrap();
+
+	deck
+}
+
+async fn update_deck(deck: &Deck, update: Document) {
+	let deck_collection = get_deck_collection().await;
+	deck_collection
+		.update_one(
+			doc! { "_id": &deck.id.unwrap() },
+			update,
+			None
+		)
+		.await
+		.unwrap();
 }
